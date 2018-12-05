159907b7fa5dfc5a4beae54ef61b07e1415026f5
Musteel <mustafa_9611@hotmail.com>
2018-12-05 22:18:51 +0100

    Books and simple menu

--- src/com/company/books.sql
+++ src/com/company/books.sql
@@ -15,21 +15,22 @@ CREATE TABLE IF NOT EXISTS `books` (
   `name` varchar(100) NOT NULL,
   `author` varchar(100) NOT NULL,
   `publisher` varchar(100) NOT NULL,
+  `category` varchar(100) NOT NULL,
   `quantity` int(10) NOT NULL,
   `issued` int(10) NOT NULL,
   `added_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
   UNIQUE KEY `callno` (`callno`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 --
 -- Data for table `books`
 --
 
-INSERT INTO `books` (`id`, `callno`, `name`, `author`, `publisher`, `quantity`, `issued`, `added_date`) VALUES
-(1, 'A@4', 'C In Depth', 'Marie', 'BPB', 2, 0, '2016-07-19'),
-(2, 'B@1', 'DBMS', 'Korth', 'CBC', 3, 0, '2016-07-18'),
-(3, 'G@12', 'Let''s see', 'Hamm', 'BPB', 10, 0, '2016-07-18');
+INSERT INTO `books` (`id`, `callno`, `name`, `author`, `publisher`, `category` , `quantity`, `issued`, `added_date`) VALUES
+(1, 'A@4', 'C In Depth', 'Marie', 'BPB', 'Educational', 2, 0, '2016-07-19'),
+(2, 'B@1', 'DBMS', 'Korth', 'CBC', 'Educational', 3, 0, '2016-07-18'),
+(3, 'G@12', 'Let''s see', 'Hamm', 'BPB', 'Comedy', 10, 0, '2016-07-18');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
